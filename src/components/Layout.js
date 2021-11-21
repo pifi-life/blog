52031429991656d2f05cf2650fbb58eb98592fc2
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 06:59:06 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -61,7 +61,6 @@ export default class Body extends React.Component {
             return <meta key={index} {...nameAttr} content={value} />;
         });
         const thumbImage= _.get(page, 'thumb_img_path');
-        const postUrl = getPageUrl(post, { withPrefix: true });
         const footer = _.get(config, 'footer');
         const hasSubscribe = _.get(footer, 'has_subscribe');
 
