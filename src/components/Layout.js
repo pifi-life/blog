74179acbc46a2649f7fbacaa2ab8e4c29b338600
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 06:46:01 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -73,6 +73,12 @@ export default class Body extends React.Component {
                     <meta name="google" content="notranslate" />
                     <meta name="title" content={seoTitle} />
                     <meta name="description" content={seoDescription} />
+                    <!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://metatags.io/">
+<meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
+<meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
                     {!_.isEmpty(seoRobots) && <meta name="robots" content={seoRobots} />}
                     {seoExtra}
                     <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700%7CRoboto:400,400i,700,700i&display=swap" rel="stylesheet" />
