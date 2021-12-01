9bae48905e35869163fb2945a46ac968a6fe2378
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 08:08:44 +0000

    fb.js: removed

--- public/js/fb.js
+++
@@ -1,8 +0,0 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        appId            : '350554330206080',
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v12.0'
-    });
-};
