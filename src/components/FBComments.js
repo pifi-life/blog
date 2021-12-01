082660a4cbd92d875e71bd38345464df35dda5d4
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 09:31:47 +0000

    FBComments.js: changed

---
+++ src/components/FBComments.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import styles from './FbComments.module.css'
+
+const FbComments = ({ url }) => {
+  useEffect(() => {
+    if(window.FB) {
+      window.FB.XFBML.parse();
+    }
+  }, [])
+  return (
+    <React.Fragment>
+      <div id='fb-root'></div>
+      <div className={`fb-comments ${styles.comments}`} data-href={url} data-width='750' data-numposts='5'></div>
+    </React.Fragment>
+  )
+}
+
+export default FbComments;
