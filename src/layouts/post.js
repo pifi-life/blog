d8bd785a8e9986f8fce1899e6f19913cac9b8eea
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 09:33:19 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import moment from 'moment-strftime';
+import { withRouter } from 'next/router'
 
 import { FBComments, Layout } from '../components/index';
 import { htmlToReact, withPrefix, markdownify } from '../utils';
