38d754294fa3573590a219d774ee71c69255560e
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 06:42:11 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,8 @@ export default class MyDocument extends Document {
         // const pageProps = _.get(this.props, '__NEXT_DATA__.props.pageProps');
         return (
             <Html {...this.helmetHtmlAttrComponents}>
-                <Head>{this.helmetHeadComponents}</Head>
+                <Head>
+                    {this.helmetHeadComponents}</Head>
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src="/js/plugins.js" />
