d2adaae22d338769aed2139ac2946c10dc70ce83
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 08:05:18 +0000

    fb.js: changed

--- public/js/fb.js
+++ public/js/fb.js
@@ -1,8 +1,8 @@
 window.fbAsyncInit = function() {
     FB.init({
-      appId            : 'your-app-id',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v12.0'
+        appId            : 'your-app-id',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v12.0'
     });
-  };
+};
