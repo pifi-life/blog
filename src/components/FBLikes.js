8d0683c49019f64a146b9e2ad838dbd370a31279
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 09:52:14 +0000

    FBLikes.js: changed

--- src/components/FBLikes.js
+++ src/components/FBLikes.js
@@ -1,6 +1,6 @@
 export const FBLikes = ({ url }) => {
     return (
-        <div class="fb-like" data-href={url} data-width="" data-layout="standard" data-action="like" data-size="large" data-share="true"></div>
+        <div class="fb-like" data-href={url} data-width="" data-layout="standard" data-action="like" data-size="small" data-share="true"></div>
     )
 }
 
